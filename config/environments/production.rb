dc691f1ad29de04c70405769e3c669286e579dde
Lucas Ingaramo <l.ingaramo@gmail.com>
2018-05-08 20:00:58 -0300

    Set default_url_options in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.default_url_options = { host: "boiling-depths-52568.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => ENV["SMTP_USERNAME"],
